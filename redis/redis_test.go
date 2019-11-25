51fbed8394ff007d7f1178bd3480c04ea1957954
Simarpreet Singh <simar@linux.com>
2019-11-24 20:24:55 -0800

    redis: Add support for ping healthcheck on creation
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -3,15 +3,29 @@ package redis
 import (
 	"testing"
 
+	"github.com/alicebob/miniredis/v2"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewStore(t *testing.T) {
-	s, err := NewStore(Options{
-		Address: "someRedisServer:1234",
+	t.Run("happy path", func(t *testing.T) {
+		mr, err := miniredis.Run()
+		assert.NoError(t, err)
+
+		s, err := NewStore(Options{
+			Address: mr.Addr(),
+		})
+
+		assert.NoError(t, err)
+		assert.Equal(t, 10000, s.p.MaxActive)
+		assert.Equal(t, 80, s.p.MaxIdle)
 	})
 
-	assert.NoError(t, err)
-	assert.Equal(t, 10000, s.p.MaxActive)
-	assert.Equal(t, 80, s.p.MaxIdle)
+	t.Run("sad path, ping fails", func(t *testing.T) {
+		s, err := NewStore(Options{
+			Address: "path/to/nowhere:1234",
+		})
+		assert.Equal(t, "redis initialization failed: dial tcp: lookup path/to/nowhere: no such host", err.Error())
+		assert.Equal(t, Store{}, s)
+	})
 }
